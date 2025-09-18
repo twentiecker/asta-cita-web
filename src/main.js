7977442cdea7b6a6b21901bb4655293e30dda44c
twentiecker <twentiecker@gmail.com>
2025-09-19 05:51:25 +0700

    integrating vueprime plugins and tailwindcss

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// primevue
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.mount("#app");
